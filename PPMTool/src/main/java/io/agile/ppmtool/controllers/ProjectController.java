486047a04fd628f3517590b45d2857194f7efd93
Mostafa-Mosad <mostafaamosad@gmail.com>
2022-01-29 16:24:58 +0200

    implement createProject feature

--- PPMTool/src/main/java/io/agile/ppmtool/controllers/ProjectController.java
+++ PPMTool/src/main/java/io/agile/ppmtool/controllers/ProjectController.java
@@ -1,7 +1,31 @@
 package io.agile.ppmtool.controllers;
 
+import io.agile.ppmtool.models.Project;
+import io.agile.ppmtool.services.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.xml.ws.Response;
+
 @RestController
+@RequestMapping("/api/project")
 public class ProjectController {
+
+    private ProjectService projectService;
+
+    @Autowired
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @PostMapping("/createproject")
+    public ResponseEntity<Project> createProject(@RequestBody Project project) {
+        Project savedProject = projectService.createProject(project);
+        return new ResponseEntity<>(savedProject, HttpStatus.CREATED);
+    }
 }
--- PPMTool/src/main/java/io/agile/ppmtool/services/ProjectService.java
+++ PPMTool/src/main/java/io/agile/ppmtool/services/ProjectService.java
@@ -1,7 +1,21 @@
 package io.agile.ppmtool.services;
 
+import io.agile.ppmtool.models.Project;
+import io.agile.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProjectService {
+
+    private ProjectRepository projectRepository;
+
+    @Autowired
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    public Project createProject(Project project) {
+        return projectRepository.save(project);
+    }
 }
