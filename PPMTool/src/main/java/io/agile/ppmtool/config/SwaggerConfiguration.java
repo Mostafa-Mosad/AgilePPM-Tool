9b7d0f0a3274071be92b7ca3373a6e6eec7f6e15
Mostafa-Mosad <mostafaamosad@gmail.com>
2022-01-29 17:10:59 +0200

    fixing merge confilct in SwaggerConfiguration

--- PPMTool/src/main/java/io/agile/ppmtool/config/SwaggerConfiguration.java
+++ PPMTool/src/main/java/io/agile/ppmtool/config/SwaggerConfiguration.java
@@ -1,4 +1,17 @@
 package io.agile.ppmtool.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
 public class SwaggerConfiguration {
+
+    @Bean
+    Docket swagger() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("io.agile.ppmtool.controllers")).build();
+    }
 }
