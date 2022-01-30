08f8d5477c11f2ba26daf9d6e07b5c201b1461bb
Mostafa-Mosad <mostafaamosad@gmail.com>
2022-01-30 12:01:09 +0200

    fix ProjectIdentifierException

--- PPMTool/src/main/java/io/agile/ppmtool/exceptions/ProjectIdentifierException.java
+++ PPMTool/src/main/java/io/agile/ppmtool/exceptions/ProjectIdentifierException.java
@@ -4,11 +4,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ProjectIdentifierException extends RuntimeException{
-
-    @ExceptionHandler(ProjectIdentifierException.class)
+public class ProjectIdentifierException extends ResponseEntityExceptionHandler {
+    
     protected ResponseEntity<Object> handleProjectIdentifierException(Exception ex) {
         return new ResponseEntity<Object>("Project Identifier already exist, please use different one!", HttpStatus.BAD_REQUEST);
     }
