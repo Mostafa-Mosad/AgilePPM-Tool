6c74cb9af206218e68f9a5e1aaafb2b01a3edf69
Mostafa-Mosad <mostafaamosad@gmail.com>
2022-01-30 11:19:17 +0200

    add validation for project model, and handle some some exceptions

--- PPMTool/src/main/java/io/agile/ppmtool/controllers/ProjectController.java
+++ PPMTool/src/main/java/io/agile/ppmtool/controllers/ProjectController.java
@@ -1,15 +1,18 @@
 package io.agile.ppmtool.controllers;
 
+import io.agile.ppmtool.exceptions.ArgumentNotValidException;
 import io.agile.ppmtool.models.Project;
 import io.agile.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import javax.xml.ws.Response;
 
 @RestController
@@ -24,7 +27,7 @@ public class ProjectController {
     }
 
     @PostMapping("/createproject")
-    public ResponseEntity<Project> createProject(@RequestBody Project project) {
+    public ResponseEntity<Project> createProject(@Valid @RequestBody Project project) {
         Project savedProject = projectService.createProject(project);
         return new ResponseEntity<>(savedProject, HttpStatus.CREATED);
     }
