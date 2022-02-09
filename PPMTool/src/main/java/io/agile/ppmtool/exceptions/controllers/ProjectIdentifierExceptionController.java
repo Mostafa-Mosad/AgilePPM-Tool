42a803ac45b88a8cc9b213e64103f0ae60213cf0
Mostafa-Mosad <mostafaamosad@gmail.com>
2022-02-09 11:43:50 +0200

    enhance exception handling

--- PPMTool/src/main/java/io/agile/ppmtool/exceptions/controllers/ProjectIdentifierExceptionController.java
+++ PPMTool/src/main/java/io/agile/ppmtool/exceptions/controllers/ProjectIdentifierExceptionController.java
@@ -1,16 +1,19 @@
 package io.agile.ppmtool.exceptions.controllers;
 
 import io.agile.ppmtool.exceptions.ProjectIdentifierException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class ProjectIdentifierExceptionController  {
 
     @ExceptionHandler(ProjectIdentifierException.class)
     protected ResponseEntity<Object> handleProjectIdentifierException(Exception ex) {
+        log.error("Project Identifier already exist, please use a different one!");
         return new ResponseEntity<Object>("Project Identifier already exist, please use a different one!", HttpStatus.BAD_REQUEST);
     }
 }
--- PPMTool/src/main/java/io/agile/ppmtool/exceptions/controllers/ProjectNotFoundExceptionController.java
+++ PPMTool/src/main/java/io/agile/ppmtool/exceptions/controllers/ProjectNotFoundExceptionController.java
@@ -1,16 +1,20 @@
 package io.agile.ppmtool.exceptions.controllers;
 
 import io.agile.ppmtool.exceptions.ProjectNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
+@Slf4j
 public class ProjectNotFoundExceptionController {
 
     @ExceptionHandler(ProjectNotFoundException.class)
     protected ResponseEntity<Object> projectNotFoundExceptionHandler(ProjectNotFoundException ex) {
+        log.error(ex.getErrorMessage());
         return new ResponseEntity<>(ex.getErrorMessage(), HttpStatus.NOT_FOUND);
     }
 }
